7bb080d41e1208a09b742d87cba557ca8dafb7cf
Pokruti <firlam@yandex.ru>
2021-07-06 18:45:54 +0300

    Lesson 2

---
+++ Lesson#02/test01/Task01/Task01/Program.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Task01
+{
+
+    public class Node
+    {
+        public int Value { get; set; }
+        public Node NextNode { get; set; }
+        public Node PrevNode { get; set; }
+    }
+
+    //Начальную и конечную ноду нужно хранить в самой реализации интерфейса
+    public interface ILinkedList
+    {
+        int GetCount(); // возвращает количество элементов в списке
+        void AddNode(int value);  // добавляет новый элемент списка
+        void AddNodeAfter(Node node, int value); // добавляет новый элемент списка после определённого элемента
+        void RemoveNode(int index); // удаляет элемент по порядковому номеру
+        void RemoveNode(Node node); // удаляет указанный элемент
+        Node FindNode(int searchValue); // ищет элемент по его значению
+    }
+
+    class TestNode : ILinkedList
+    {
+        Node First;
+        Node Last;
+        int count;
+
+        public void AddNode(int value)
+        {
+            Node node = new Node();
+            node.Value = value;
+            if (First == null)
+            {
+                First = node;
+            }
+            else
+            {
+                Last.NextNode = node;
+                node.PrevNode = Last;
+            }
+
+            Last = node;
+            count++;
+            GetCount();
+
+
+        }
+
+        public void AddNodeAfter(Node node, int value)
+
+        {
+
+            Node afternode = new Node();
+            Node tempnode = new Node();
+
+            afternode.Value = value;
+            if (First == null)
+            {
+                First = afternode;
+            }
+            else
+            {
+
+                node.NextNode = afternode;
+                afternode.PrevNode = node;
+                afternode.NextNode = tempnode;
+                tempnode.PrevNode = afternode;
+
+
+                count++;
+
+            }
+            GetCount();
+
+        }
+
+
+
+        public Node FindNode(int searchValue)
+        {
+
+            Node srcnode = new Node();
+            srcnode.Value = First.Value;
+            while (srcnode.Value != searchValue)
+            {
+                srcnode = srcnode.NextNode;
+            }
+            return srcnode;
+
+        }
+
+        public int GetCount()
+        {
+            return count;
+        }
+
+        public void RemoveNode(int index)
+        {
+            Node temp = new Node();
+            if (index == 1)
+            {
+                if (First == null)
+                {
+                    throw new InvalidOperationException();
+                }
+                else
+                {
+
+                    if (First.NextNode != null)
+                    {
+                        First.NextNode.PrevNode = null;
+                    }
+                    First = First.NextNode;
+                    count--;
+                    GetCount();
+                }
+            }
+            else
+            {
+                if (Last == null)
+                {
+                    throw new InvalidOperationException();
+                }
+                else
+                {
+                    int i = 0;
+                    temp = First;
+                    while (i < index)
+                    {
+                        temp = temp.NextNode;
+                        i++;
+                    }
+
+                    if (temp.PrevNode != null)
+                    {
+                        temp.PrevNode.NextNode = temp.NextNode;
+                        temp.NextNode.PrevNode = temp.PrevNode;
+
+                    }
+
+                    count--;
+                    GetCount();
+                }
+            }
+
+        }
+
+        public void RemoveNode(Node node)
+        {
+            Node remnode = new Node();
+            remnode = First;
+            while (remnode != node)
+            {
+                remnode = remnode.NextNode;
+            }
+            remnode.PrevNode.NextNode = remnode.NextNode;
+            remnode.NextNode.PrevNode = remnode.PrevNode;
+            count--;
+            GetCount();
+
+        }
+    }
+
+
+class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+        }
+    }
+}
---
+++ Lesson#02/test02/Task02/Task02/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Task02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        public static int BinarySearch(int[] inputArray, int searchValue)
+        {
+            int min = 0;
+            int max = inputArray.Length - 1;
+            while (min <= max)
+            {
+                int mid = (min + max) / 2;
+                if (searchValue == inputArray[mid])
+                {
+                    return mid;                             // N/(2*2*2*2*.....*2)
+                }                                           // N/2^X
+                else if (searchValue < inputArray[mid])     //2^X = N
+                {                                           //log2(2^X) = log2(N)	
+                    max = mid - 1;                          //x*log2(2) = log2(N)
+                }                                           //x*1 = log2(N)
+                else                                        //O(log2(N))
+                {
+                    min = mid + 1;
+                }
+            }
+            return -1;
+        }
+    }
+}
